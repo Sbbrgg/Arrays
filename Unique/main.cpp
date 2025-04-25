20f87d15def93fab3ecfb0abef918737ae9b7426
xromo <xromo@DESKTOP-J6CQG2O>
2025-04-25 21:05:14 +0300

    unique DONE

--- Unique/main.cpp
+++ Unique/main.cpp
@@ -1,15 +1,20 @@
 ﻿#include <iostream>
 using namespace std;
 
+#define tab "\t"
+
+//#define HOMEWORK
+
 void main()
 {
 	setlocale(LC_ALL, "Russian");
+#ifdef HOMEWORK
 	const int n = 10;
 	int array[n] = { 0 }, minRand = 70, maxRand = 80, value = 0;
 	for (int i = 0; i < n; i++)
 	{
 		value = rand() % (maxRand - minRand) + minRand;
-		bool check = true;	
+		bool check = true;
 		for (int j = 0; j < i; j++)
 		{
 			if (value == array[j])
@@ -19,7 +24,7 @@ void main()
 				break;
 			}
 		}
-		if (check) 
+		if (check)
 		{
 			array[i] = value;
 		}
@@ -28,4 +33,30 @@ void main()
 	{
 		cout << array[i] << " ";
 	}
+#endif // HOMEWORK
+	const int n = 10;
+	int arr[n];
+	for (int i = 0; i < n; i++)
+	{
+		bool unique;
+		do
+		{
+			arr[i] = rand() % 10;
+			unique = true;	//предположим, что сгенерировалось уникальное случайное число
+							//но это нужно проверить
+			for (int j = 0; j < i; j++)
+			{
+				if (arr[i] == arr[j])
+				{
+					unique = false;
+					break;
+				}
+			}
+		} while (!unique);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
 }
