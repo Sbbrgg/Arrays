e5b1da262c34f21d974b04ae4496fb1d70b63a99
xromo <xromo@DESKTOP-J6CQG2O>
2025-04-25 20:49:01 +0300

    Numerics

--- Numerics/main.cpp
+++ Numerics/main.cpp
@@ -1,9 +1,13 @@
 ﻿#include <iostream>
-using namespace std;
-
+using std::cout;
+using std::cin;
+using std::endl;
+//#define HOMEWORK
+//#define BINARY
 void main()
 {
 	setlocale(LC_ALL, "Russian");
+#ifdef HOMEWORK
 	int number;
 	cout << "Введите десятичное число: "; cin >> number; cout << endl;
 	cout << "Двоичное представление числа: ";
@@ -23,7 +27,7 @@ void main()
 	}
 	cout << endl << "Шестнадцатиричное представление числа: ";
 	if (number == 0) cout << 0;
-	else 
+	else
 	{
 		char array_of_binary[32] = { 0 };
 		int i = 0;
@@ -36,7 +40,7 @@ void main()
 			{
 				array_of_binary[i] = '0' + remainder;
 			}
-			else 
+			else
 			{
 				array_of_binary[i] = 'A' + (remainder - 10);
 			}
@@ -45,4 +49,36 @@ void main()
 		}
 		for (int j = i - 1; j >= 0; j--)cout << array_of_binary[j];
 	}
+#endif // HOMEWORK
+	int decimal;	//эта переменная будет хранить десятичное число, введённое пользователем
+	cout << "Введите десятичное число: "; cin >> decimal;
+#ifdef BINARY
+	const int n = 32;	//максимальная возможная разрядность двоичного числа
+	bool binary[n] = { 0 };		//этот массив будет хранить разряды двоичного числа
+	int i = 0;
+	for (; decimal; decimal >>= 1) // decimal > 0
+		binary[i++] = decimal & 1;
+	for (--i; i >= 0; i--)
+	{
+		cout << binary[i];
+		if (i % 8 == 0)cout << " ";
+		if (i % 4 == 0)cout << " ";
+	}
+	cout << endl;
+#endif // BINARY
+	const int n = 8;
+	char hex[n] = {};
+	int i = 0;
+	for (; decimal; decimal >>= 4)
+	{
+		hex[i] = decimal & 15;
+		hex[i++] += hex[i] < 10 ? 48 : 55;
+	}
+
+	for (--i; i >= 0; i--)
+		cout << hex[i];
+		//cout << (char)(hex[i] < 10 ? hex[i] + 48 : hex[i] + 55);
+	cout << endl;
+
+
 }
